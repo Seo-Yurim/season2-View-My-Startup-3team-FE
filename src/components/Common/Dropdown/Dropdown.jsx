9ff84cb00c83ec019479cbd452b54ee376e29141
Seo-Yurim <yurim2204@naver.com>
2025-04-14 17:36:44 +0900

    refactor: 정렬 수정

--- src/components/Common/Dropdown/Dropdown.jsx
+++ src/components/Common/Dropdown/Dropdown.jsx
@@ -2,17 +2,12 @@ import styles from "./Dropdown.module.css";
 import arrowDown from "../../../assets/ic_toggle.svg";
 import { useState, useRef, useEffect } from "react";
 
-const options = {
-  "View My Startup 누적 투자 금액 높은 순": ["sim_invest", "desc"],
-  "View My Startup 누적 투자 금액 낮은 순": ["sim_invest", "asc"],
-  "실제 누적 투자 금액 높은 순": ["actual_invest", "desc"],
-  "실제 누적 투자 금액 낮은 순": ["actual_invest", "asc"],
-};
-
-export default function Dropdown({ setSortOrder }) {
+export default function Dropdown({ sortOptions, setSortOrder, order, sort }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [currentLabel, setCurrentLabel] = useState(Object.keys(options)[0]);
   const dropdownRef = useRef(null);
+  const currentLabel = Object.entries(sortOptions).find(
+    ([, value]) => value[0] === order && value[1] === sort
+  )?.[0];
 
   // 메뉴 외부 클릭 감지
   useEffect(() => {
@@ -32,23 +27,20 @@ export default function Dropdown({ setSortOrder }) {
   }, [dropdownRef]);
 
   const handleOptionClick = (val) => {
-    const [orderValue, sortValue] = options[val];
+    const [orderValue, sortValue] = sortOptions[val];
     setSortOrder(orderValue, sortValue);
-    setCurrentLabel(val);
     setIsOpen(false);
   };
 
   return (
     <div className={styles.menu} ref={dropdownRef}>
       <div className={styles.selected} onClick={() => setIsOpen(!isOpen)}>
-        <p className={styles.currentLabel}>
-          {currentLabel || Object.keys(options)[0]}
-        </p>
+        <p className={styles.currentLabel}>{currentLabel}</p>
         <img className={styles.icon} src={arrowDown} alt="드롭다운 아이콘" />
       </div>
       {isOpen && (
         <ul className={styles.options}>
-          {Object.keys(options).map((option, idx) => (
+          {Object.keys(sortOptions).map((option, idx) => (
             <li
               key={idx}
               className={styles.option}
---
+++ src/pages/InvestmentPage.jsx
@@ -0,0 +1,64 @@
+import InvestmentList from "../components/Investment/InvestmentList";
+import { useState } from "react";
+import { useGetInvestmentList } from "../api/queries/investmentQuery";
+import Pagination from "../components/Common/Pagination";
+import Dropdown from "../components/Common/Dropdown/Dropdown";
+import styles from "../styles/InvestmentPage.module.css";
+
+const sortOptions = {
+  "View My Startup 누적 투자 금액 높은 순": ["sim_invest", "desc"],
+  "View My Startup 누적 투자 금액 낮은 순": ["sim_invest", "asc"],
+  "실제 누적 투자 금액 높은 순": ["actual_invest", "desc"],
+  "실제 누적 투자 금액 낮은 순": ["actual_invest", "asc"],
+};
+
+export default function InvestmentPage() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+  const [params, setParams] = useState({
+    order: "sim_invest",
+    sort: "desc",
+  });
+
+  const { data, isLoading, isError, isFetching } = useGetInvestmentList({
+    currentPage,
+    pageSize,
+    order: params.order,
+    sort: params.sort,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error..</div>;
+
+  const list = data.list;
+  const totalPages = Math.ceil(data.totalCount / pageSize);
+
+  // 정렬 처리 함수
+  const handleSortChange = (order, sort) => {
+    setParams((prevParams) => ({
+      ...prevParams,
+      order,
+      sort,
+    }));
+  };
+
+  return (
+    <div>
+      <div className={styles.header}>
+        <h1 className={styles.title}>투자 현황</h1>
+        <Dropdown
+          sortOptions={sortOptions}
+          setSortOrder={handleSortChange}
+          order={params.order}
+          sort={params.sort}
+        />
+      </div>
+      <InvestmentList list={list} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
+    </div>
+  );
+}
