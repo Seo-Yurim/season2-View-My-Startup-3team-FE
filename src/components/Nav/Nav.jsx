4c9cd18b788332f3c88ebec63a019d4678361d2d
Seo-Yurim <yurim2204@naver.com>
2025-04-21 21:25:23 +0900

    refactor: 모바일 메뉴 버튼 구현

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,9 @@
 import styles from "./Nav.module.css";
 import siteLogo from "../../assets/img_logo_pc.svg";
+import mobileMenu from "../../assets/ic_menu_md.svg";
 import { useLocation, useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function getLinkStyle({ isActive }) {
   return {
@@ -14,6 +16,8 @@ export default function Nav() {
   const location = useLocation();
   const navigate = useNavigate();
 
+  const [dropdownOopen, setDropdownOpen] = useState(false);
+
   const hanleLogoClick = () => {
     if (location.pathname === "/") {
       window.location.reload();
@@ -24,36 +28,63 @@ export default function Nav() {
 
   return (
     <header className={styles.header}>
-      <img
-        className={styles.logo}
-        src={siteLogo}
-        onClick={hanleLogoClick}
-        alt="View My Startup Logo"
-      />
-      <nav>
-        <ul className={styles.menu}>
-          <li>
-            <NavLink to="/startup" style={getLinkStyle}>
-              전체 스타트업 목록
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/my-comparison" style={getLinkStyle}>
-              나의 기업 비교
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/comparison" style={getLinkStyle}>
-              비교 현황
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/investment" style={getLinkStyle}>
-              투자 현황
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.wrap}>
+        <div className={styles.images}>
+          <img
+            className={styles.logo}
+            src={siteLogo}
+            onClick={hanleLogoClick}
+            alt="View My Startup Logo"
+          />
+          <img
+            className={styles[`mobile-menu`]}
+            src={mobileMenu}
+            onClick={() => setDropdownOpen((prev) => !prev)}
+            alt="mobile menu"
+          />
+        </div>
+
+        <nav className={`${styles.nav} ${dropdownOopen ? styles.show : ""}`}>
+          <ul className={styles.menu}>
+            <li>
+              <NavLink
+                to="/startup"
+                style={getLinkStyle}
+                onClick={() => setDropdownOpen(false)}
+              >
+                전체 스타트업 목록
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/my-comparison"
+                style={getLinkStyle}
+                onClick={() => setDropdownOpen(false)}
+              >
+                나의 기업 비교
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/comparison"
+                style={getLinkStyle}
+                onClick={() => setDropdownOpen(false)}
+              >
+                비교 현황
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/investment"
+                style={getLinkStyle}
+                onClick={() => setDropdownOpen(false)}
+              >
+                투자 현황
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
