0f0661136ea345ccaa63d373ab987b9db21ed5b6
Seo-Yurim <yurim2204@naver.com>
2025-04-21 18:07:44 +0900

    refactor: 컨테이너 컴포넌트에 시맨틱 태그 추가 및 화면 전환 애니메이션 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,17 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Nav from "./Nav/Nav";
 import Container from "./Common/Container/Container";
 import Footer from "./Footer/Footer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
 
 const queryClient = new QueryClient();
 
 export default function App() {
-  const location = useLocation();
-  const [fadeKey, setFadeKey] = useState(0);
-
-  useEffect(() => {
-    setFadeKey((prev) => prev + 1);
-  }, [location.pathname]);
-
   return (
     <QueryClientProvider client={queryClient}>
       <Nav />
       <Container>
-        <div key={fadeKey} className="fade-transition">
-          <Outlet />
-        </div>
+        <Outlet />
       </Container>
       <Footer />
     </QueryClientProvider>
--- src/components/Common/Container/Container.jsx
+++ src/components/Common/Container/Container.jsx
@@ -1,5 +1,18 @@
-import styles from './Container.module.css';
+import styles from "./Container.module.css";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Container({ children }) {
-  return <div className={styles.container}>{children}</div>;
+  const location = useLocation();
+  const [fadeKey, setFadeKey] = useState(0);
+
+  useEffect(() => {
+    setFadeKey((prev) => prev + 1);
+  }, [location.pathname]);
+
+  return (
+    <main key={fadeKey} className={styles.container}>
+      {children}
+    </main>
+  );
 }
