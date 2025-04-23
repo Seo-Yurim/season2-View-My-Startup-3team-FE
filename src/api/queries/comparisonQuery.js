9a7a526f38921b0c5f57b5546e50678fab8ab6ad
Seo-Yurim <yurim2204@naver.com>
2025-04-23 21:24:27 +0900

    feat: 비교 관련 쿼리 작성

---
+++ src/api/queries/comparisonQuery.js
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import { getCompareResult, getRecentSelection } from '../ComparisonService';
+
+// 최근 선택된 스타트업 목록 조회
+export const useGetRecentSelectionList = () => {
+  return useQuery({
+    queryKey: ['recnet-selection'],
+    queryFn: () => getRecentSelection(),
+    keepPreviousData: true
+  });
+};
+
+// 선택한 스타트업 비교 결과 조회
+export const useGetCompareResult = ({ sessionId, orderBy, sortBy }) => {
+  return useQuery({
+    queryKey: ['compare-result', sessionId, orderBy, sortBy],
+    queryFn: () => getCompareResult({ sessionId, orderBy, sortBy }),
+    keepPreviousData: true
+  });
+};
---
+++ src/api/queries/selectionQuery.js
@@ -0,0 +1,83 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {
+  cancelCompareStartups,
+  cancelMyStartup,
+  getSeletedStartups,
+  selectCompareStartups,
+  selectMyStartup
+} from '../SelectionService';
+
+// 세션에 저장된 스타트업 불러오기
+export const useGetSelectedStartups = ({ sessionId }) => {
+  return useQuery({
+    queryKey: ['selected-startups', sessionId],
+    queryFn: () => getSeletedStartups({ sessionId }),
+    keepPreviousData: true
+  });
+};
+
+// 나의 스타트업 선택
+export const useSelectMyStartup = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ id, sessionId }) => selectMyStartup({ id, sessionId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['startup-list']
+      });
+    },
+    onError: (error) => {
+      console.error('내 기업 선택에 실패하였습니다.', error.message);
+    }
+  });
+};
+
+// 비교할 기업들 선택
+export const useSelectCompareStartups = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ ids, sessionId }) =>
+      selectCompareStartups({ ids, sessionId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['startup-list']
+      });
+    },
+    onError: (error) => {
+      console.error('비교 기업 선택에 실패하였습니다.', error.message);
+    }
+  });
+};
+
+// 나의 스타트업 선택 취소
+export const useCancelMyStartup = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ id, sessionId }) => cancelMyStartup({ id, sessionId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['startup-list']
+      });
+    },
+    onError: (error) => {
+      console.error('선택한 기업 취소에 실패하였습니다.', error.message);
+    }
+  });
+};
+
+// 비교할 기업 선택 취소
+export const useCancelCompreStartups = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ ids, sessionId }) =>
+      cancelCompareStartups({ ids, sessionId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['startup-list']
+      });
+    },
+    onError: (error) => {
+      console.error('선택한 기업 취소에 실패하였습니다.', error.message);
+    }
+  });
+};
