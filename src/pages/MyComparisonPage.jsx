1a30cf9496a08deeca3df99172dcb0aa223bcabc
Seo-Yurim <yurim2204@naver.com>
2025-04-28 10:37:30 +0900

    refactor: meta태그 추가

--- src/pages/MyComparisonPage.jsx
+++ src/pages/MyComparisonPage.jsx
@@ -13,6 +13,7 @@ import RankResult from "../components/Selection/RankResult";
 import CompareResult from "../components/Selection/CompareResult";
 import MySelection from "../components/Selection/MySelection";
 import CompareSelection from "../components/Selection/CompareSelection";
+import { Helmet } from "react-helmet-async";
 
 export default function MyComparisonPage() {
   let sessionId = sessionStorage.getItem("sessionId");
@@ -57,64 +58,88 @@ export default function MyComparisonPage() {
   };
 
   return (
-    <main className={styles.wrap}>
-      {isLoading && <Loading />}
-      {isError && (
-        <Warn
-          variant="error"
-          title="오류발생"
-          description={"데이터를 불러오는 중 오류가 발생했습니다."}
+    <>
+      <Helmet>
+        <title>나의 기업 비교 - View My Startup</title>
+        <meta
+          name="description"
+          content="나의 기업과 다른 기업을 비교해보세요!"
         />
-      )}
-      {!isLoading && !isError && (
-        <>
-          <MySelection
-            sessionId={sessionId}
-            selectedStartup={selectedStartup}
-            compareStartups={compareStartups}
-            onReset={handleReset}
-            isComparisonDone={isComparisonDone}
-            setIsComparisonDone={setIsComparisonDone}
+        <meta property="og:title" content="나의 기업 비교" />
+        <meta
+          property="og:description"
+          content="나의 기업과 다른 기업을 비교해보세요!"
+        />
+        <meta
+          property="og:url"
+          content="http://view-my-startup-s3-fe.s3-website.ap-northeast-2.amazonaws.com/my-comparison"
+        />
+      </Helmet>
+
+      <main className={styles.wrap}>
+        {isLoading && <Loading />}
+        {isError && (
+          <Warn
+            variant="error"
+            title="오류발생"
+            description={"데이터를 불러오는 중 오류가 발생했습니다."}
           />
-          {!isComparisonDone &&
-            (selectedStartup || compareStartups.length > 0) && (
-              <CompareSelection
-                sessionId={sessionId}
-                selectedStartup={selectedStartup}
-                compareSelectedStartups={compareStartups}
-                setCompareStartups={setCompareStartups}
-              />
-            )}
-        </>
-      )}
+        )}
+        {!isLoading && !isError && (
+          <>
+            <MySelection
+              sessionId={sessionId}
+              selectedStartup={selectedStartup}
+              compareStartups={compareStartups}
+              onReset={handleReset}
+              isComparisonDone={isComparisonDone}
+              setIsComparisonDone={setIsComparisonDone}
+            />
+            {!isComparisonDone &&
+              (selectedStartup || compareStartups.length > 0) && (
+                <CompareSelection
+                  sessionId={sessionId}
+                  selectedStartup={selectedStartup}
+                  compareSelectedStartups={compareStartups}
+                  setCompareStartups={setCompareStartups}
+                />
+              )}
+          </>
+        )}
 
-      {!isComparisonDone && (
-        <Button
-          width="20rem"
-          label="기업 비교하기"
-          onClick={handleShowResult}
-          isDisabled={selectedStartup && compareStartups.length ? false : true}
-        />
-      )}
-
-      {isComparisonDone && (
-        <div className={styles.result}>
-          <CompareResult sessionId={sessionId} startupId={selectedStartup.id} />
-          <RankResult startupId={selectedStartup.id} />
+        {!isComparisonDone && (
           <Button
-            label="나의 기업에 투자하기"
             width="20rem"
-            onClick={() => setIsInvestModal(true)}
+            label="기업 비교하기"
+            onClick={handleShowResult}
+            isDisabled={
+              selectedStartup && compareStartups.length ? false : true
+            }
           />
-        </div>
-      )}
+        )}
 
-      {isInvestModal && (
-        <CreateInvestModal
-          onClose={() => setIsInvestModal(false)}
-          startup={selectedStartup}
-        />
-      )}
-    </main>
+        {isComparisonDone && (
+          <div className={styles.result}>
+            <CompareResult
+              sessionId={sessionId}
+              startupId={selectedStartup.id}
+            />
+            <RankResult startupId={selectedStartup.id} />
+            <Button
+              label="나의 기업에 투자하기"
+              width="20rem"
+              onClick={() => setIsInvestModal(true)}
+            />
+          </div>
+        )}
+
+        {isInvestModal && (
+          <CreateInvestModal
+            onClose={() => setIsInvestModal(false)}
+            startup={selectedStartup}
+          />
+        )}
+      </main>
+    </>
   );
 }
